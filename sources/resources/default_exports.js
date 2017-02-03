d70eaf9e67f942ec70978b87e399c63bd5c8ac96
Michael Osei <mosei27@live.com>
2017-02-03 16:55:22 -0600

    Update default theme

---
+++ sources/resources/default_exports.js
@@ -0,0 +1,14 @@
+/**
+  Default Eye candy graphs and bars for various stats
+
+  STILL WORKING ON THIS FEATURE
+**/
+namespace Graphs{
+    class HorizontalBar
+    {
+        function draw(){
+            console.log("Draw graph\n");
+        }
+    }
+
+}
